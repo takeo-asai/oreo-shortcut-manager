1ee06a66d66cf99588a0441f09526c523f1aea6a
Takeo Asai <spitson+github@gmail.com>
2017-09-02 16:57:46 +0900

    Implement Pinend Shortcut for Oreo

--- app/src/main/java/com/github/spitson/takeo/myshortcut/MainActivity.kt
+++ app/src/main/java/com/github/spitson/takeo/myshortcut/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.github.spitson.takeo.myshortcut
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.view.View
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openShortcut(view: View) {
+        val intent = Intent(this, ShortcutActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun createShortcut(view: View) {
+        val manager = getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager
+        val intent = Intent(this, ShortcutActivity::class.java)
+        intent.action = Intent.ACTION_VIEW
+
+        val info = ShortcutInfo.Builder(this, "shortcut-id")
+                .setShortLabel("label")
+                .setIcon(Icon.createWithResource(this, R.drawable.abc_ic_ab_back_material))
+                .setIntent(intent)
+                .build()
+        manager.requestPinShortcut(info, null)
+
     }
 }
---
+++ app/src/main/java/com/github/spitson/takeo/myshortcut/ShortcutActivity.kt
@@ -0,0 +1,12 @@
+package com.github.spitson.takeo.myshortcut
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ShortcutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shortcut)
+    }
+}
